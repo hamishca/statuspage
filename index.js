70771a430c1a700e98512b8ea8cc9cf4a7923c87
Vijaye Raji <vijaye@statsig.com>
2023-09-07 08:43:16 +1000

    [Automated] Update Health Check Logs

--- index.js
+++ index.js
@@ -250,3 +250,28 @@ async function genAllReports() {
     await genReportLog(document.getElementById("reports"), key, url);
   }
 }
+
+async function genIncidentReport() {
+  const response = await fetch(
+    "https://incidents.statsig.workers.dev/contents"
+  );
+  if (response.ok) {
+    const json = await response.json();
+    try {
+      const activeDom = DOMPurify.sanitize(
+        marked.parse(json.active ? json.active : "No active incidents")
+      );
+      const inactiveDom = DOMPurify.sanitize(marked.parse(json.inactive));
+      document.getElementById("activeIncidentReports").innerHTML = activeDom;
+      document.getElementById("pastIncidentReports").innerHTML = inactiveDom;
+
+      if (json.active) {
+        setTimeout(() => {
+          document.getElementById("incidents").scrollIntoView(true);
+        }, 1000);
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+}
